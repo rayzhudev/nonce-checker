cc57815cc4054c61f3269bacbcd4515755f767be
Ray Zhu <rayzhudev@gmail.com>
2024-12-07 10:24:06 +1100

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Handle GitHub Pages path
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
